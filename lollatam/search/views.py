94ed07242ae31163122f5ac5cc4ddb9a0a7d3764
Williams Jiménez <127707891+williamsjmzhdz@users.noreply.github.com>
2023-03-20 19:36:20 -0600

    I created the index.js file and the form in index.html.

--- lollatam/search/views.py
+++ lollatam/search/views.py
@@ -5,14 +5,17 @@ from django.http import HttpResponseNotAllowed
 
 from users.models import Profile
 
+from lollatam.secrets import RIOT_API_KEY
+
 
 def index(request):
+
+    context = {}
+
     if request.user.is_authenticated:
         if request.method == 'GET':
             profile = Profile.objects.get(user=request.user)
-            context = {
-                'email_verified': profile.email_verified,
-            }
+            context['email_verified'] = profile.email_verified
             return render(request, 'search/index.html', context)
         else:
             return HttpResponseNotAllowed(['GET'])
